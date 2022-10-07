b88bf75867ca54a9ce24603f35c0e3fcc50c4b99
David <Kudavidss@gmail.com>
2022-10-07 19:34:35 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,22 +1,12 @@
 const settings = {
-  interval: 10,
+  interval: 5,
   emoji: '🚀',
   text: [
     'Test 1',
     'Test 2',
     'Test 3',
     'Test 4',
-    'Test 5',
-    'Test 6',
-    'Test 7',
-    'Test 8',
-    'Test 9',
-    'Test 10',
-    'Test 11',
-    'Test 12',
-    'Test 13',
-    'Test 14',
-    'Test 15'
+    'Test 5'
   ]
 };
 
